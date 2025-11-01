70ebcc9140fc1194b7efb48e925001d550445d30
Aniket Mahakalkar <mahakalkaraniket001@gmail.com>
2025-11-01 14:27:46 +0530

    database setup is done and working on creating controllers

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	controller "github.com/golang_fullstack_ai_movie_streaming_app/backend/server/controllers"
+)
 
 func main() {
 
-	fmt.Println("Hello , go world")
+	router := gin.Default()
+
+	router.GET("/ping", func(ctx *gin.Context) {
+
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+
+	})
+
+	router.GET("/movies", controller.GetMovies())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server",err)
+	}
 }
